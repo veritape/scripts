dcc340e346cd25b6959724beca02c53d5e0da297
aniversarioperu <yoni@aniversarioperu.me>
2015-03-29 00:16:47 +0200

    agregar demandantes

--- pull_data_from_redam_pages.py
+++ pull_data_from_redam_pages.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 import codecs
 import json
-import re
-import locale
 import glob
-import sys
+import re
 
 from bs4 import BeautifulSoup
 
-
-'''
+"""
 Extrae nombres, apellidos, dni y monto que deben los ciudadanos por
 concepto de juicio por alimentos. Info tomada de REDAM usando curl.
 Este script actúa sobre archivos HTML.
-'''
+
+curl 'http://casillas.pj.gob.pe/redamWeb/_rlvid.jsp.faces?_rap=pc_Index.obtenerDeudor&_rvip=/index.jsp&idDeudor=[1-3000]' -o '#1.html'
+"""
 
 
 def to_number(string):
@@ -31,7 +30,6 @@ def extract_name(filename):
     apellido_materno = soup.find(id='form1:text157').get_text()
     dni = soup.find(id='form1:text158').get_text()
     debe = soup.find_all('span', id=re.compile(".+textNimpadeudado1"))
-    deudas = []
     dinero = 0
     for i in debe:
         dinero += to_number(i.get_text())
@@ -43,10 +41,28 @@ def extract_name(filename):
     obj['dni'] = dni
     obj['debe'] = dinero
     obj['url'] = "http://casillas.pj.gob.pe/redamWeb/_rlvid.jsp.faces?_rap=pc_Index.obtenerDeudor&_rvip=/index.jsp&idDeudor=" + this_id
+    obj['vinculo'] = get_vinculo(soup)
     return obj
+
+
+def get_vinculo(soup):
+    """
+    Devuelve datos de persona vinculada a nuestro usuario. Ejemplo:
+    - demandante, nombre completo
+    """
+    vinculos = []
+    items = soup.find_all(id=re.compile('form1:tableEx[0-9]:[0-9]:tableEx4.0+'))
+    for item in items:
+        if 'vinculo' in item['id']:
+            vinculo = item.get_text()
+        if 'Nombre' in item['id']:
+            nombre_completo = item.get_text()
+            vinculos.append({'vinculo': vinculo, 'nombre_completo': nombre_completo})
+
+    return list({v['vinculo']: v for v in vinculos}.values())
 
 
 for filename in glob.glob("*html"):
     obj = extract_name(filename)
-    with codecs.open("deudores_redam.txt", "a", "utf-8") as f:
+    with codecs.open("deudores_redam.json", "a", "utf-8") as f:
         f.write(json.dumps(obj) + "\n")
